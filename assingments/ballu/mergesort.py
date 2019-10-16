76755b29a176e1ef5ebe4c938f73aea66426d905
Pappu1523 <suresh.munduri457@gmail.com>
2019-10-16 12:11:59 +0530

    added mergesort

--- assingments/ballu/mergesort.py
+++ assingments/ballu/mergesort.py
@@ -8,9 +8,7 @@ def mergeSort(alist):
         mergeSort(lefthalf)
         mergeSort(righthalf)
 
-        i=0
-        j=0
-        k=0
+        i=j=k=0
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] <= righthalf[j]:
                 alist[k]=lefthalf[i]
