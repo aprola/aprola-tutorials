cead885782f49b3c09016ff683163a1f327aea6a
Pappu1523 <suresh.munduri457@gmail.com>
2019-10-19 16:14:47 +0530

    counting sort changed

--- assingments/ballu/countingsort.py
+++ assingments/ballu/countingsort.py
@@ -1,5 +1,5 @@
-def counting_sort(arr1, max_value):
-    m = max_value + 1
+def counting_sort(arr1, max):
+    m = max + 1
     count = [0] * m
 
     for a in arr1:
@@ -12,4 +12,3 @@ def counting_sort(arr1, max_value):
             i += 1
     return arr1   
 
-print(counting_sort( [1, 2, 7, 3, 2, 1, 4, 2, 3, 2, 1], 7 ))
