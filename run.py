354307df567272da67704f797a78c24aafa25c15
Shireesh Kumar <g.shireesh.kumar@gmail.com>
2019-10-20 01:08:06 +0530

    heap sort v1

--- run.py
+++ run.py
@@ -1,18 +1,22 @@
-from utils.random import randomArrayGenerator2
+from utils.random import randomArrayGeneratorWithoutDuplicates
 from utils.random import randomArrayGeneratorWithDuplicates
 from sorting.mergesort import mergesort
 from sorting.quicksort import quicksort
 from sorting.radix import radix
 from sorting.counting import countingsort
+from sorting.heapsort import heapsort, heapsort_geeks_for_geeks
 from assingments.nishu.mergesort import mergeSort
 from assingments.ballu.countingsort import counting_sort
 
 
 
-nos = int(input("enter sample size: "))
-max = int(input("enter max value: "))
-sample = randomArrayGeneratorWithDuplicates(nos, 0, max)
+# nos = int(input("enter sample size: "))
+# max = int(input("enter max value: "))
+# sample = randomArrayGeneratorWithDuplicates(nos, 0, max)
+# sample = randomArrayGeneratorWithoutDuplicates(nos)
+sample = [4, 8, 0, 3, 2, 1, 6, 7, 5, 9]
 # sample = [3,1,2,5,0,4]
 print("sample is : ", sample)
-result = counting_sort(sample,nos-1)
-print("sorted sample is : ", result)
+# heapsort_geeks_for_geeks(sample)
+heapsort(sample)
+print("sorted sample is : ", sample)
--- utils/random.py
+++ utils/random.py
@@ -1,4 +1,15 @@
 import random
+import time
+
+# last_print_at = time.time()
+# interval = 4
+
+# def print_at_interval(*val):
+#     if int((last_print_at%3600)/interval) != int((time.time() % 3600)/interval):
+#         now = time.time()
+#         print(*val)
+#         last_print_at = now
+    
 
 def randomArrayGeneratorWithDuplicates(nos, min=0, max=1000):
     values = []
@@ -7,13 +18,15 @@ def randomArrayGeneratorWithDuplicates(nos, min=0, max=1000):
         y = random.randint(min, max)
         values.append(y)
         x -= 1
+        # print_at_interval("percent: ", 100*(nos-x)/nos)
     return values
 
 
-def randomArrayGenerator2(nos):
+def randomArrayGeneratorWithoutDuplicates(nos):
     values = []
     for x in range(nos):
         y = len(values)
         pos = random.randint(0, y)
+        # print_at_interval("percent: ", 100*x/nos)
         values.insert(pos, x)
     return values
