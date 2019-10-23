67d860d8b41121b786a2f36419fbfeea3824b0b0
Shireesh Kumar <g.shireesh.kumar@gmail.com>
2019-10-23 22:47:05 +0530

    added changes as per review comments

--- assingments/linsifa/quicksort.py
+++ assingments/linsifa/quicksort.py
@@ -1,78 +1,42 @@
-arr = [54, 7, 25, 2, 6, 92, 8, 12, 13]
+# sample
+# arr = [54, 7, 25, 2, 6, 92, 8, 12, 13]
 
 # values.sort()
-
 # print("sorted values: ", values)
 def partition(arr,low,high):
-
     i = ( low-1 )         # index of smaller element
-
     pivot = arr[high]     # pivot
-
-  
-
     for j in range(low , high):
-
-  
-
         # If current element is smaller than or
-
         # equal to pivot
-
         if   arr[j] < pivot:
-
-          
-
             # increment index of smaller element
-
             i = i+1
-
             arr[i],arr[j] = arr[j],arr[i]
-
-  
-
     arr[i+1],arr[high] = arr[high],arr[i+1]
-
     return ( i+1 )
 
   
 
 # The main function that implements QuickSort
-
 # arr[] --> Array to be sorted,
-
 # low  --> Starting index,
-
 # high  --> Ending index
 
-  
 
 # Function to do Quick sort
+def quickSort(arr,low=0,high=-1):
 
-def quickSort(arr,low,high):
-
+    if high == -1:
+        high = len(arr) - 1
     if low < high:
-
-  
-
         # pi is partitioning index, arr[p] is now
-
         # at right place
-
         pi = partition(arr,low,high)
-
-  
-
         # Separately sort elements before
-
         # partition and after partition
-
         quickSort(arr, low, pi-1)
-
         quickSort(arr, pi+1, high)
 
-       
-
-quickSort(arr, 0, len(arr)-1)
-
-print("sorted values: ", arr)
+# usage
+# quickSort(arr, 0, len(arr)-1)
